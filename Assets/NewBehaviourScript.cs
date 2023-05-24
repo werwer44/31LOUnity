49560a643d6afdd3751f3e3e60ddb406088e48b2
werwer44 <nikodemradwan@gmail.com>
2023-05-24 16:53:01 +0200

    autoreset

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform transformToReset;
+
+public Vector3 startingPosition;
+
+public Vector3 startingRotation; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingposition = transformToReset.position
+        startingposition = transformToReset.rotation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
